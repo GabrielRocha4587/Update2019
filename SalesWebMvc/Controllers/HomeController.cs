43081eda650a20f57ca48a14bb2ff5d8cf6a3b13
GabrielRocha4587 <gabrieltorrescoelho@gmail.com>
2026-09-18 18:29:01 -0300

    CRUD Scaffolding

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -33,6 +33,8 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+       
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
